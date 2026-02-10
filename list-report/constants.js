69816624fe151f4d0db360a293ceaa3164eec6e9
Facu Siracusa <faq885@gmail.com>
2026-02-10 16:55:56 +0100

    test batch

--- list-report/constants.js
+++ list-report/constants.js
@@ -1,7 +1,7 @@
 const APP_KEY = "f583659eec153297d82610f3b624b731";
 const APP_NAME = "Case List Report";
 const ICON_URL = "https://cdn-icons-png.flaticon.com/512/3029/3029337.png";
-const VERSION = 1.1;
+const VERSION = 1.2;
 
 // Set to true to fetch cards one-by-one (more reliable for large lists); false to use batched fetch.
-const USE_SINGLE_CARD_FETCH = true;
+const USE_SINGLE_CARD_FETCH = false;
